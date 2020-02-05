49c7d0dc6c048c1da260fb9e13329aa83f8465b1
Priyanka Thakur <thakurp@usf.edu>
2020-02-04 21:04:35 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace exercise3_properties
+{
+    class Person
+    {
+        private string name; // field
+        public string Name   // property
+        {
+            get { return name; }
+            set { name = value; }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person myObj = new Person();
+            myObj.Name = "Priyanka";
+            Console.WriteLine(myObj.Name);
+        }
+    }
+}
